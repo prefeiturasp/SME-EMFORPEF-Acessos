4226ebd71ff091351bde92e740eca827922f7d99
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-06-12 10:05:55 -0300

    Revert "Ajuste da rota"
    This reverts commit 2db5c5be2605c222fe5adf31cb9b3dcbed08f93e.

--- SME.Acessos.Api/Controllers/AutenticacaoCdepController.cs
+++ SME.Acessos.Api/Controllers/AutenticacaoCdepController.cs
@@ -6,6 +6,7 @@ using SME.Acessos.Infra.Dominio.CoreSSO;
 
 namespace SME.Acessos.Api.Controllers
 {
+    [Route("api/v1/autenticacao-cdep")]
     [ApiController]
     public class AutenticacaoCdepController : BaseController
     {
