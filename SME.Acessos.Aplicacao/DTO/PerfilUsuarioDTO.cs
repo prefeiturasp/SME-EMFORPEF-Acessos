e7d87d9ec6e450f513a597b47ab311c9bc019f5b
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-06-14 09:29:49 -0300

    Removendo o construtor

--- SME.Acessos.Aplicacao/DTO/PerfilUsuarioDTO.cs
+++ SME.Acessos.Aplicacao/DTO/PerfilUsuarioDTO.cs
@@ -3,11 +3,6 @@ namespace SME.Acessos.Aplicacao.DTO
 {
     public struct PerfilUsuarioDTO
     {
-        public PerfilUsuarioDTO(Guid perfil, string perfilNome)
-        {
-            Perfil = perfil;
-            PerfilNome = perfilNome;
-        }
         public Guid Perfil { get; set; }
         public string PerfilNome { get; set; }
     }
