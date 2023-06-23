79b5083aee0d9b977e1c72ad20087a1f10674a95
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-06-23 14:33:06 -0300

    Criar script - tabela de Recuperação de senha - Acessos

---
+++ SME.Acessos.Infra.Dominio/Acessos/Entidades/UsuarioRecuperacaoToken.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.Acessos.Infra.Dominio.Acessos.Entidades
+{
+    public class UsuarioRecuperacaoToken : EntidadeBaseAcessos
+    {
+        public string Login { get; set; }
+        public DateTime? ExpiracaoRecuperacaoSenha { get; set; }
+        public Guid? TokenRecuperacaoSenha { get; set; }
+    }
+}
---
+++ SME.Acessos.Infra.Dominio/Acessos/Repositorios/IRepositorioUsuarioRecuperacaoToken.cs
@@ -0,0 +1,10 @@
+﻿
+using SME.Acessos.Infra.Dominio.CoreSSO.Entidades;
+
+namespace SME.Acessos.Infra.Dominio.Acessos.Repositorios;
+
+public interface IRepositorioUsuarioRecuperacaoToken
+{
+    Task<Usuario> ObterUsuarioPorLogin(string login);
+    Task<Usuario> ObterUsuarioPorTokenRecuperacaoSenha(Guid token);
+}
