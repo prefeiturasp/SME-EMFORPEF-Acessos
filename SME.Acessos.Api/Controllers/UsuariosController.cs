8cfb73d603a0d20983928cfc7109f4219bc64846
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-06-28 16:03:38 -0300

    Ajuste de status dos endpoints

--- SME.Acessos.Api/Controllers/UsuariosController.cs
+++ SME.Acessos.Api/Controllers/UsuariosController.cs
@@ -12,6 +12,7 @@ namespace SME.Acessos.Api.Controllers
         [HttpPost("cadastrar")]
         [ProducesResponseType(typeof(bool), 200)]
         [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         public async Task<IActionResult> Autenticar([FromBody] UsuarioDTO usuarioDto, [FromServices] IServicoUsuarios servicoUsuarios)
         {
             var retornoAutenticacao = await servicoUsuarios.Cadastrar(usuarioDto);
@@ -22,6 +23,7 @@ namespace SME.Acessos.Api.Controllers
         [HttpGet("{login}/cadastrado")]
         [ProducesResponseType(typeof(bool), 200)]
         [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         public async Task<IActionResult> UsuarioCadastradoCoreSSO([FromRoute] string login, [FromServices] IServicoUsuarios servicoUsuarios)
         {
             var retorno = await servicoUsuarios.UsuarioCadastradoCoreSSO(login);
@@ -32,6 +34,7 @@ namespace SME.Acessos.Api.Controllers
         [HttpPost("{login}/vincular-perfil/{perfilId}")]
         [ProducesResponseType(typeof(bool), 200)]
         [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         public async Task<IActionResult> VincularPerfil([FromRoute] string login, Guid perfilId, [FromServices] IServicoUsuarioGrupo servicoUsuarioGrupo)
         {
             var retorno = await servicoUsuarioGrupo.VincularPerfil(login,perfilId);
@@ -42,6 +45,7 @@ namespace SME.Acessos.Api.Controllers
         [HttpGet("{login}")]
         [ProducesResponseType(typeof(DadosUsuarioDTO), 200)]
         [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         [ProducesResponseType(601)]
         public async Task<IActionResult> MeusDados([FromRoute] string login, [FromServices] IServicoUsuarios servicoUsuarios)
         {
@@ -53,7 +57,7 @@ namespace SME.Acessos.Api.Controllers
         [HttpPut("{login}/senha")]
         [ProducesResponseType(typeof(bool), 200)]
         [ProducesResponseType(400)]
-        [ProducesResponseType(500)]
+        [ProducesResponseType(401)]
         public async Task<IActionResult> AlterarSenha([FromRoute] string login,[FromBody] AlterarSenhaUsuarioDTO alterarSenhaUsuarioDto, [FromServices] IServicoUsuarios servicoUsuarios)
         {
             var retorno = await servicoUsuarios.AlterarSenha(login,alterarSenhaUsuarioDto);
@@ -64,7 +68,7 @@ namespace SME.Acessos.Api.Controllers
         [HttpPut("{login}/email")]
         [ProducesResponseType(typeof(bool), 200)]
         [ProducesResponseType(400)]
-        [ProducesResponseType(500)]
+        [ProducesResponseType(401)]
         public async Task<IActionResult> AlterarEmail([FromRoute] string login, [FromBody] AlterarEmailUsuarioDTO alterarEmailUsuarioDto, [FromServices] IServicoUsuarios servicoUsuarios)
         {
             var retorno = await servicoUsuarios.AlterarEmail(login, alterarEmailUsuarioDto);
@@ -75,6 +79,7 @@ namespace SME.Acessos.Api.Controllers
         [HttpGet("{login}/sistemas/{sistemaId}/recuperar-senha")] 
         [ProducesResponseType(typeof(string), 200)]
         [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         [ProducesResponseType(601)]
         public async Task<IActionResult> SolicitarRecuperacaoSenha([FromRoute] string login, long sistemaId, [FromServices] IServicoUsuarios servicoUsuarios)
         {
@@ -84,7 +89,7 @@ namespace SME.Acessos.Api.Controllers
         [HttpGet("{token}/sistemas/{sistemaId}/validar")] 
         [ProducesResponseType(typeof(bool), 200)]
         [ProducesResponseType(400)]
-        [ProducesResponseType(500)]
+        [ProducesResponseType(401)]
         public async Task<IActionResult> TokenRecuperacaoSenhaEstaValido([FromRoute] Guid token, long sistemaId, [FromServices] IServicoUsuarios servicoUsuarios)
         {
             return Ok(await servicoUsuarios.ValidarTokenRecuperacaoSenha(token, sistemaId));
@@ -93,6 +98,7 @@ namespace SME.Acessos.Api.Controllers
         [HttpPut("sistemas/{sistemaId}/senha")] 
         [ProducesResponseType(typeof(RetornoAlteracaoSenhaDto), 200)]
         [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         [ProducesResponseType(601)]
         public async Task<IActionResult> AlterarSenhaComTokenRecuperacao([FromRoute] long sistemaId,[FromBody] AlterarSenhaPorTokenDto alterarSenha, [FromServices] IServicoUsuarios servicoUsuarios)
         {
@@ -102,23 +108,16 @@ namespace SME.Acessos.Api.Controllers
         
         private IActionResult TratarRetornoAlterarSenha(RetornoAlteracaoSenhaDto retornoAlterar)
         {
-            switch (retornoAlterar.Status)
+            return retornoAlterar.Status switch
             {
-                case AlterarSenhaStatus.TokenExpirado:
-                    return Unauthorized(MensagemNegocio.TOKEN_INVALIDO_OU_EXPIRADO);
-                case AlterarSenhaStatus.ForaPadrao:
-                    return Unauthorized(MensagemNegocio.SENHA_FORA_DO_PADRAO);
-                case AlterarSenhaStatus.NaoEncontrado:
-                    return Unauthorized(MensagemNegocio.USUARIO_OU_SENHA_INCORRETOS);
-                case AlterarSenhaStatus.OK:
-                    return Ok(retornoAlterar.Login);
-                case AlterarSenhaStatus.NoHistorico:
-                    return Unauthorized(MensagemNegocio.A_SENHA_NAO_PODE_SER_UMA_DAS_ULTIMAS_5_ANTERIORES);
-                case AlterarSenhaStatus.SenhaPadrao:
-                    return Unauthorized(MensagemNegocio.A_NOVA_SENHA_NAO_PODE_SER_UMA_SENHA_PADRAO);
-                default:
-                    return Ok(retornoAlterar.Login);
-            }
+                AlterarSenhaStatus.TokenExpirado => Unauthorized(MensagemNegocio.TOKEN_INVALIDO_OU_EXPIRADO),
+                AlterarSenhaStatus.ForaPadrao => Unauthorized(MensagemNegocio.SENHA_FORA_DO_PADRAO),
+                AlterarSenhaStatus.NaoEncontrado => Unauthorized(MensagemNegocio.USUARIO_OU_SENHA_INCORRETOS),
+                AlterarSenhaStatus.OK => Ok(retornoAlterar.Login),
+                AlterarSenhaStatus.NoHistorico => Unauthorized(MensagemNegocio.A_SENHA_NAO_PODE_SER_UMA_DAS_ULTIMAS_5_ANTERIORES),
+                AlterarSenhaStatus.SenhaPadrao => Unauthorized(MensagemNegocio.A_NOVA_SENHA_NAO_PODE_SER_UMA_SENHA_PADRAO),
+                _ => Ok(retornoAlterar.Login)
+            };
         }
     }
 }
