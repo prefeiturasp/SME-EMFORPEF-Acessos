5cd968875b1c03c0be4561988a70fb677b6e4600
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-07-04 15:15:26 -0300

    Ajuste de rota

--- SME.Acessos.Api/Controllers/UsuariosController.cs
+++ SME.Acessos.Api/Controllers/UsuariosController.cs
@@ -76,7 +76,7 @@ namespace SME.Acessos.Api.Controllers
         }
         
         [HttpGet("{login}/sistemas/{sistemaId}/recuperar-senha")] 
-        [ProducesResponseType(typeof(string), 200,"Retorna o e-mail do usuário que solicitou a recuperação de senha")]
+        [ProducesResponseType(typeof(string), 200)]
         [ProducesResponseType(400)]
         [ProducesResponseType(401)]
         [ProducesResponseType(601)]
