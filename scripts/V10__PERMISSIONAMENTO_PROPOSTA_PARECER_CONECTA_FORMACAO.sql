947fbdbfb6ec712b0c78c8c1147e9e37cc41c938
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-11-23 09:18:57 -0300

    [Back] Criar permissão no coresso - modulos e permissões

---
+++ scripts/V10__PERMISSIONAMENTO_PROPOSTA_PARECER_CONECTA_FORMACAO.sql
@@ -0,0 +1,34 @@
+﻿--> Módulos
+
+--> Atribuir proposta para gestão
+insert into modulos (id, descricao, idmodcoresso,idacao) 	
+select (select coalesce(max(id)+1,1) from modulos),'Atribuir proposta para gestão',5,2
+where not exists (select 1 from modulos where descricao = 'Atribuir proposta para gestão');
+
+--> Dar parecer da proposta
+insert into modulos (id, descricao, idmodcoresso,idacao) 	
+select (select max(id)+1 from modulos),'Dar parecer da proposta',6,2
+where not exists (select 1 from modulos where descricao = 'Dar parecer da proposta');
+
+--> Devolver proposta
+insert into modulos (id, descricao, idmodcoresso,idacao) 	
+select (select max(id)+1 from modulos),'Devolver proposta',7,2
+where not exists (select 1 from modulos where descricao = 'Devolver proposta');
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--> Permissões - Admin DF
+
+--> Atribuir proposta para gestão
+insert into permissoes (idgrupo, idmodulo) 	
+select (select id from grupos where nome = 'Admin DF'),id 
+from modulos where idmodcoresso = 5 and not exists (select 1 from permissoes where idmodulo in (select id from modulos where idmodcoresso = 5));
+
+--> Dar parecer da proposta
+insert into permissoes (idgrupo, idmodulo) 	
+select (select id from grupos where nome = 'Admin DF'),id 
+from modulos where idmodcoresso = 6 and not exists (select 1 from permissoes where idmodulo in (select id from modulos where idmodcoresso = 6));
+
+--> Devolver proposta
+insert into permissoes (idgrupo, idmodulo) 	
+select (select id from grupos where nome = 'Admin DF'),id 
+from modulos where idmodcoresso = 7 and not exists (select 1 from permissoes where idmodulo in (select id from modulos where idmodcoresso = 7));
