714930d28d00d4702a40105c99419054edb4c74a
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-11-24 09:49:40 -0300

    Adicionando Perfis e Dres ao token

--- SME.Acessos.Infra.Dados/Repositorios/CoreSSO/RepositorioUsuario.cs
+++ SME.Acessos.Infra.Dados/Repositorios/CoreSSO/RepositorioUsuario.cs
@@ -99,5 +99,18 @@ namespace SME.Acessos.Infra.Dados.Repositorios.CoreSSO
                                    where usu_id = @usuarioId ";
             await conexao.Obter().ExecuteAsync(alterarEmail, new {usuarioId, email});
         }
+        
+        public async Task<IEnumerable<string>> ObterDresPorLogin(string login)
+        {
+            var query = @"select distinct ua.uad_codigo 
+                          from SYS_UsuarioGrupoUA ug
+                            join SYS_UnidadeAdministrativa ua on ua.uad_id = ug.uad_id 
+                            join SYS_Usuario su on su.usu_id = ug.usu_id 
+                          where su.usu_login = @login ";
+
+            var usuarios = await conexao.Obter().QueryAsync<string>(query, new { login });
+            
+            return usuarios;
+        }
     }
 }
--- SME.Acessos.TesteIntegracao/ServicosFakes/RepositorioUsuarioCoreSSOFake.cs
+++ SME.Acessos.TesteIntegracao/ServicosFakes/RepositorioUsuarioCoreSSOFake.cs
@@ -83,5 +83,10 @@ namespace SME.Acessos.TesteIntegracao.ServicosFakes
         {
             return Task.CompletedTask;
         }
+
+        public Task<IEnumerable<string>> ObterDresPorLogin(string login)
+        {
+            return Task.FromResult<IEnumerable<string>>(new List<string> { "1", "2", "3", "4" });
+        }
     }
 }
