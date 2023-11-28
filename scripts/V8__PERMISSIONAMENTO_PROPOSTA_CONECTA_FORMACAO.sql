4084197938708b0b97e9c3c29c41336e1a43c45d
Juliano Martins <juliano.campos@amcom.com.br>
2023-11-28 14:20:54 -0400

    ajuste modulo proposta

--- scripts/V8__PERMISSIONAMENTO_PROPOSTA_CONECTA_FORMACAO.sql
+++ scripts/V8__PERMISSIONAMENTO_PROPOSTA_CONECTA_FORMACAO.sql
@@ -2,19 +2,19 @@
 
 --> Proposta
 insert into modulos (id, descricao, idmodcoresso,idacao, idsistemacoresso) 	
-select (select coalesce(max(id)+1,1) from modulos),'Proposta - Consulta',3,1,1007
+select (select coalesce(max(id)+1,1) from modulos),'Proposta - Consulta',4,1,1007
 where not exists (select 1 from modulos where descricao = 'Proposta - Consulta');
 
 insert into modulos (id, descricao, idmodcoresso,idacao, idsistemacoresso) 	
-select (select max(id)+1 from modulos),'Proposta - Inclusão',3,2,1007
+select (select max(id)+1 from modulos),'Proposta - Inclusão',4,2,1007
 where not exists (select 1 from modulos where descricao = 'Proposta - Inclusão');
 
 insert into modulos (id, descricao, idmodcoresso,idacao, idsistemacoresso) 	
-select (select max(id)+1 from modulos),'Proposta - Exclusão',3,3,1007
+select (select max(id)+1 from modulos),'Proposta - Exclusão',4,3,1007
 where not exists (select 1 from modulos where descricao = 'Proposta - Exclusão');
 
 insert into modulos (id, descricao, idmodcoresso,idacao, idsistemacoresso) 	
-select (select max(id)+1 from modulos),'Proposta - Alteração',3,4,1007
+select (select max(id)+1 from modulos),'Proposta - Alteração',4,4,1007
 where not exists (select 1 from modulos where descricao = 'Proposta - Alteração');
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -23,4 +23,4 @@ where not exists (select 1 from modulos where descricao = 'Proposta - Alteraçã
 --> Proposta
 insert into permissoes (idgrupo, idmodulo) 	
 select (select id from grupos where nome = 'Admin DF'),id 
-from modulos where idmodcoresso = 3 and idsistemacoresso =  1007 and not exists (select 1 from permissoes where idmodulo in (select id from modulos where idmodcoresso = 3 and idsistemacoresso =  1007));
+from modulos where idmodcoresso = 4 and idsistemacoresso =  1007 and not exists (select 1 from permissoes where idmodulo in (select id from modulos where idmodcoresso = 4 and idsistemacoresso =  1007));
