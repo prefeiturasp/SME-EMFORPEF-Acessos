d0515f24a1e0a50fa4305542260b04d6fc2104d5
Juliano Martins <juliano.campos@amcom.com.br>
2023-11-28 14:16:50 -0400

    revertendo funcionalidade história cancelada

--- scripts_coresso/V100__INCLUIR_GRUPOS_GESTAO_CONECTA_FORMACAO.sql
+++ scripts_coresso/V100__INCLUIR_GRUPOS_GESTAO_CONECTA_FORMACAO.sql
@@ -7,47 +7,7 @@ BEGIN TRY
 		where ss.sis_nome = 'Conecta Formação';
 			  
 	    insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'DF',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'DF');
-		
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão DIEFEM',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão DIEFEM');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão DIEE',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão DIEE');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão DIEI',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão DIEI');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão NAAPA SME',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão NAAPA SME');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão DIEJA',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão DIEJA');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão DA',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão DA');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão Multimeios',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão Multimeios');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão DC',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão DC');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão NAC',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão NAC');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão COCEU',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão COCEU');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão CODAE',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão CODAE');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão DIPED',1,getdate(),3,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão DIPED');
-
-		insert into SYS_Grupo(gru_nome, gru_situacao,gru_dataCriacao, vis_id, sis_id)
-	    select 'Gestão DICEU',1,getdate(),3,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'Gestão DICEU');
-				
+	    select 'DF',1,getdate(),1,@sistema_id where not exists (select * from sys_grupo where gru_nome = 'DF');		
 	 COMMIT TRAN;
 END TRY
 BEGIN CATCH
