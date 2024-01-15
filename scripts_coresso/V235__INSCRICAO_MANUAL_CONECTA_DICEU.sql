6da9d1c44835ef0cff5e29853d6fc98aaabbb611
Thiago Fernando <thiago.fernando@msn.com>
2024-01-15 10:33:51 -0300

    Correção de Perfil DICEU e SINPEEM

--- scripts_coresso/V235__INSCRICAO_MANUAL_CONECTA_DICEU.sql
+++ scripts_coresso/V235__INSCRICAO_MANUAL_CONECTA_DICEU.sql
@@ -2,7 +2,7 @@ Declare @sistema_id int;
 declare @grupo_perfil_id uniqueidentifier;
 Declare @mod_id_pai_perfil int;
 Declare @mod_id int = 6;
-Declare @grupo_nome varchar(max) = 'DICEU'
+Declare @grupo_nome varchar(max) = 'DICEU (DRE)'
 
 
 BEGIN TRY
