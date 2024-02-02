018679e559973fb7c61bf216a4a73706b1b83b22
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-02-02 14:40:52 -0300

    Link do recuperar, obtém direto do banco

--- SME.Acessos.Aplicacao/Servicos/ServicoUsuarios.cs
+++ SME.Acessos.Aplicacao/Servicos/ServicoUsuarios.cs
@@ -176,7 +176,7 @@ namespace SME.Acessos.Aplicacao
             var textoEmail = textoArquivo
                 .Replace("#NOME", usuario.Pessoa.Nome)
                 .Replace("#RF", login)
-                .Replace("#LINK", string.Format(sistema.Endereco,token,sistema.CodigoSistema, sistema.TipoAcao));
+                .Replace("#LINK", sistema.Endereco);
 
             await servicoEmail.Enviar(usuario.Pessoa.Nome, usuario.Email, $"Recuperação de senha do(a) {sistema.NomeSistema}", textoEmail, sistema.CodigoSistema);
         }
