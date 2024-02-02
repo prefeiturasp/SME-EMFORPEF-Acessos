9a233dfa950e71f040129af283c4ca5c4affb0db
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-02-02 16:02:22 -0300

    Ajuste nos registros do banco

--- scripts/V16__ALTER_TABLE_SISTEMA_RECUPERACAO_SENHA.sql
+++ scripts/V16__ALTER_TABLE_SISTEMA_RECUPERACAO_SENHA.sql
@@ -12,6 +12,14 @@ insert into sistema_acao (codigo_sistema, nome_sistema, endereco, tipo)
 select '1007', 'Conecta Formação', 'https://conectaformacao.sme.prefeitura.sp.gov.br/validar-email/{token}', 2
 where not exists (select id from sistema_acao where codigo_sistema = '1007' and tipo = 2);
 
+--> Colocar token no sistema_acao
+update sistema_acao set endereco = 'https://novosgp.sme.prefeitura.sp.gov.br/redefinir-senha/{0}' where codigo_sistema = 1 and tipo = 1;
+update sistema_acao set endereco = 'https://intranet.sme.prefeitura.sp.gov.br/index.php/nova-senha/?token={0}' where codigo_sistema = 2 and tipo = 1;
+update sistema_acao set endereco = 'https://sigpae.sme.prefeitura.sp.gov.br/redefinir-senha/{0}' where codigo_sistema = 3 and tipo = 1;
+update sistema_acao set endereco = 'https://cdep.sme.prefeitura.sp.gov.br/redefinir-senha/{0}' where codigo_sistema = 1006 and tipo = 1;
+update sistema_acao set endereco = 'https://conectaformacao.sme.prefeitura.sp.gov.br/redefinir-senha/{0}' where codigo_sistema = 1007 and tipo = 1;
+update sistema_acao set endereco = 'https://conectaformacao.sme.prefeitura.sp.gov.br/validar-email/{0}' where codigo_sistema = 1007 and tipo = 2;
+
 --> Renomear tabela de 'usuario_recuperacao_senha' para 'usuario_validacao_token'
 alter table usuario_recuperacao_senha rename to usuario_validacao_token;
 
