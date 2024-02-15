430c6e85274520bd515cc54d3bd885af22c0b0fb
Thiago Fernando <thiago.fernando@msn.com>
2024-02-15 14:55:48 -0300

    correção no login

--- SME.Acessos.Infra.Dados/Repositorios/CoreSSO/RepositorioUsuario.cs
+++ SME.Acessos.Infra.Dados/Repositorios/CoreSSO/RepositorioUsuario.cs
@@ -14,7 +14,7 @@ namespace SME.Acessos.Infra.Dados.Repositorios.CoreSSO
 
         public async Task<Usuario> ObterPorLogin(string login)
         {
-            var query = @"select * from (select usu_id, 
+            var query = @" select usu_id, 
                                  usu_login, 
                                  usu_email, 
                                  usu_senha,
@@ -25,8 +25,7 @@ namespace SME.Acessos.Infra.Dados.Repositorios.CoreSSO
                          join pes_pessoa p on u.pes_id = p.pes_id
                          left join pes_pessoadocumento pd on p.pes_id = pd.pes_id
                          and pd.tdo_id = @tipoDocumentoCpf
-                        where u.usu_login = @login ) as consulta
-                         where psd_numero = @login ";
+                        where u.usu_login = @login ";
             
             var usuarios = await conexao.Obter().QueryAsync<Usuario, Pessoa, PessoaDocumento,Usuario>(query, 
                 (usuario, pessoa, pessoaDocumento) =>
