ab9b06380c87e2b10423e0a8c19a5c0c228661ea
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-02-19 15:13:38 -0300

    Ajuste de permissões do CDEP

--- scripts/V17__PERMISSIONAMENTO_GESTAO_VISITA_CALENDARIO_CDEP.sql
+++ scripts/V17__PERMISSIONAMENTO_GESTAO_VISITA_CALENDARIO_CDEP.sql
@@ -43,3 +43,5 @@ insert into permissoes (idgrupo, idmodulo)
 select (select id from grupos where nome = 'Básico'),id 
 from modulos where idmodcoresso = 11 and not exists (select 1 from permissoes where idmodulo in (select id from modulos where idmodcoresso = 11 and idacao = 1));
 
+update modulos set idsistemacoresso = 1006 where id in (304,303,302,301,300,299,298,297);
+
