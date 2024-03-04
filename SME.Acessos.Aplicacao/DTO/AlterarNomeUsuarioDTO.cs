99ab9640f738c8de4ab3e5c8aec831aea952ed46
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-03-04 18:23:45 -0300

    [BACK] Criar endpoint para Alterar o nome do usuário no CORESSO

---
+++ SME.Acessos.Aplicacao/DTO/AlterarNomeUsuarioDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SME.Acessos.Aplicacao.DTO
+{
+    public class AlterarNomeUsuarioDTO
+    {
+        [Required(ErrorMessage = "É necessário informar o nome")]
+        public string Nome { get; set; }
+    }
+}
--- SME.Acessos.Aplicacao/Interfaces/IServicoUsuarios.cs
+++ SME.Acessos.Aplicacao/Interfaces/IServicoUsuarios.cs
@@ -20,5 +20,6 @@ namespace SME.Acessos.Aplicacao.Interfaces
         Task<RetornoAlteracaoSenhaDto> AlterarSenhaPorToken(long sistemaId, AlterarSenhaPorTokenDto alterarSenha);
         Task<IEnumerable<ResponsavelDTO>> ObterUsuariosComPerfisResponsavel(Guid[] perfis, long sistemaId);
         Task<bool> EnviarEmailValidacaoCadastro(string login, long sistemaId);
+        Task<bool> AlterarNome(string login, string nome);
     }
 }
