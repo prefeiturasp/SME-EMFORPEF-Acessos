8799e8adbb735972958ffb76bea2c03d4eeca38c
Thiago Fernando <thiago.fernando@msn.com>
2024-04-22 16:44:12 -0300

    Criado endpoint para consulta parecerista

--- SME.Acessos.Api/Controllers/UsuariosController.cs
+++ SME.Acessos.Api/Controllers/UsuariosController.cs
@@ -168,5 +168,15 @@ namespace SME.Acessos.Api.Controllers
 
             return Ok(retorno);
         }
+
+        [HttpGet("usuarios-pareristas")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public async Task<IActionResult> ObterPareceristas([FromRoute] string login, string nome, [FromServices] IServicoPerfilUsuario servico)
+        {
+            var retorno = await servico.ObterUsuariosPerfilPareceristasConecta(login, nome);
+            return Ok(retorno);
+        }
+        
     }
 }
