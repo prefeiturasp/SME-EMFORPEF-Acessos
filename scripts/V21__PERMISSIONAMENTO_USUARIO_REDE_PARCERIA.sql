e7f6727aaa12130fd15c9c4b0354cd06fafc90aa
Juliano Martins <juliano.campos@amcom.com.br>
2024-05-14 13:55:03 -0400

    Código do módulo

--- scripts/V21__PERMISSIONAMENTO_USUARIO_REDE_PARCERIA.sql
+++ scripts/V21__PERMISSIONAMENTO_USUARIO_REDE_PARCERIA.sql
@@ -1,20 +1,20 @@
 ﻿--> Módulos
 
 --> Rede de Parceria (Usuários)
-insert into modulos (id, descricao, idmodcoresso,idacao) 	
-select (select coalesce(max(id)+1,1) from modulos),'Rede de Parceria (Usuários) - Consulta',2,1
+insert into modulos (id, descricao, idmodcoresso, idacao, idsistemacoresso) 	
+select (select coalesce(max(id)+1,1) from modulos),'Rede de Parceria (Usuários) - Consulta',39,1,1007
 where not exists (select 1 from modulos where descricao = 'Rede de Parceria (Usuários) - Consulta');
 
-insert into modulos (id, descricao, idmodcoresso,idacao) 	
-select (select max(id)+1 from modulos),'Rede de Parceria (Usuários) - Inclusão',2,2
+insert into modulos (id, descricao, idmodcoresso, idacao, idsistemacoresso) 	
+select (select max(id)+1 from modulos),'Rede de Parceria (Usuários) - Inclusão',39,2,1007
 where not exists (select 1 from modulos where descricao = 'Rede de Parceria (Usuários) - Inclusão');
 
-insert into modulos (id, descricao, idmodcoresso,idacao) 	
-select (select max(id)+1 from modulos),'Rede de Parceria (Usuários) - Exclusão',2,3
+insert into modulos (id, descricao, idmodcoresso, idacao, idsistemacoresso) 	
+select (select max(id)+1 from modulos),'Rede de Parceria (Usuários) - Exclusão',39,3,1007
 where not exists (select 1 from modulos where descricao = 'Rede de Parceria (Usuários) - Exclusão');
 
-insert into modulos (id, descricao, idmodcoresso,idacao) 	
-select (select max(id)+1 from modulos),'Rede de Parceria (Usuários) - Alteração',2,4
+insert into modulos (id, descricao, idmodcoresso, idacao, idsistemacoresso) 	
+select (select max(id)+1 from modulos),'Rede de Parceria (Usuários) - Alteração',39,4,1007
 where not exists (select 1 from modulos where descricao = 'Rede de Parceria (Usuários) - Alteração');
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -23,4 +23,4 @@ where not exists (select 1 from modulos where descricao = 'Rede de Parceria (Usu
 --> Rede de Parceria (Usuários)
 insert into permissoes (idgrupo, idmodulo) 	
 select (select id from grupos where nome = 'Admin DF'),id 
-from modulos where idmodcoresso = 2 and not exists (select 1 from permissoes where idmodulo in (select id from modulos where idmodcoresso = 2));
+from modulos where idmodcoresso = 39 and not exists (select 1 from permissoes where idmodulo in (select id from modulos where idmodcoresso = 2));
