61b6d96140c2473c24e2cedce3a2c4c93ebf00b2
Diego Moreno <dierudito@gmail.com>
2026-03-02 17:01:35 -0300

    feat: Adiciona endpoint para carga de usuário NÃO SUBIR PARA PRD
    AB#144144

---
+++ src/SME.Acessos.Aplicacao/Servicos/ServicoUsuarioTeste.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+using SME.Acessos.Aplicacao.DTO;
+using SME.Acessos.Aplicacao.Interfaces;
+
+namespace SME.Acessos.Aplicacao.Servicos
+{
+    public class ServicoUsuarioTeste(IServicoUsuarios servicoUsuarios, IServicoUsuarioGrupo servicoUsuarioGrupo) : IServicoUsuarioTeste
+    {
+        private static readonly Faker _faker = new("pt_BR");
+        public async Task<IEnumerable<DadosPessoaUsuarioDto>> CadastrarUsuariosEmMassaAsync(int quantidade, Guid? perfilId)
+        {
+            perfilId ??= new Guid("7EDA4540-A16C-4FE5-8322-9F75B3414E27");
+
+            var usuariosCadastrados = new List<DadosPessoaUsuarioDto>(quantidade);
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                var usuario = GerarUsuarioDeTeste();
+                if (!await servicoUsuarios.Cadastrar(usuario)) continue;
+                if (!await servicoUsuarioGrupo.VincularPerfil(usuario.Login, perfilId.Value)) continue;
+                usuariosCadastrados.Add(new DadosPessoaUsuarioDto()
+                {
+                    Login = usuario.Login,
+                    Nome = usuario.Nome,
+                    Email = usuario.Email,
+                    Senha = usuario.Senha,
+                    PerfilId = perfilId.Value
+                });
+            }
+            return usuariosCadastrados;
+        }
+        private static UsuarioDTO GerarUsuarioDeTeste()
+        {
+            return new UsuarioDTO()
+            {
+                Login = _faker.Internet.UserName(),
+                Nome = _faker.Name.FullName(),
+                Email = _faker.Internet.Email(),
+                Senha = _faker.Internet.Password()
+            };
+        }
+    }
+}
